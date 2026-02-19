f8d5b4d322e4b15d2869120eb5622fe14aeb13d7
Globox1997 <Micha.Damaschke@web.de>
2026-02-19 16:59:32 +0100

    finished hopper cart

--- src/main/java/net/driller/mixin/MinecartHopperMixin.java
+++ src/main/java/net/driller/mixin/MinecartHopperMixin.java
@@ -1,17 +1,16 @@
 package net.driller.mixin;
 
 import net.driller.access.HopperAccess;
-import net.driller.mixin.access.AbstractMinecartContainerAccessor;
-import net.minecraft.client.gui.screens.inventory.HopperScreen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.*;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
@@ -20,7 +19,6 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.AbstractMinecartContainer;
 import net.minecraft.world.entity.vehicle.MinecartHopper;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.PoweredRailBlock;
@@ -57,30 +55,6 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
         super.defineSynchedData(builder);
         builder.define(LEFT_HOPPER, false);
         builder.define(RIGHT_HOPPER, false);
-    }
-
-    @Inject(method = "getDefaultDisplayOffset", at = @At("RETURN"), cancellable = true)
-    private void getDefaultDisplayOffsetMixin(CallbackInfoReturnable<Integer> info) {
-        int offset = info.getReturnValue();
-        if (!this.hoppersInventory.getItem(0).isEmpty()) {
-            offset += 1;
-        }
-        if (!this.hoppersInventory.getItem(1).isEmpty()) {
-            offset += 1;
-        }
-        info.setReturnValue(offset);
-    }
-
-    @Inject(method = "getContainerSize", at = @At("RETURN"), cancellable = true)
-    private void getContainerSizeMixin(CallbackInfoReturnable<Integer> info) {
-        int size = info.getReturnValue();
-        if (!this.hoppersInventory.getItem(0).isEmpty()) {
-            size += 5;
-        }
-        if (!this.hoppersInventory.getItem(1).isEmpty()) {
-            size += 5;
-        }
-        info.setReturnValue(size);
     }
 
     @Inject(method = "suckInItems", at = @At("RETURN"), cancellable = true)
@@ -160,16 +134,6 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
         }
         this.entityData.set(LEFT_HOPPER, !this.hoppersInventory.getItem(0).isEmpty());
         this.entityData.set(RIGHT_HOPPER, !this.hoppersInventory.getItem(1).isEmpty());
-
-        System.out.println("TEST TEST "+this.getContainerSize());
-
-        if (this instanceof AbstractMinecartContainerAccessor abstractMinecartContainerAccessor) {
-            NonNullList<ItemStack> nonNullList = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-            for (int i = 0; i < this.getContainerSize(); i++) {
-                nonNullList.set(i, abstractMinecartContainerAccessor.driller$getItemStacks().get(i));
-            }
-            abstractMinecartContainerAccessor.driller$setItemStacks(nonNullList);
-        }
     }
 
     @Override
@@ -197,15 +161,6 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
                     this.hoppersInventory.setItem(1, player.getItemInHand(interactionHand).copy());
                     this.entityData.set(RIGHT_HOPPER, true);
 
-                    if (this instanceof AbstractMinecartContainerAccessor abstractMinecartContainerAccessor) {
-                        NonNullList<ItemStack> nonNullList = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-
-                        for (int i = 0; i < this.getContainerSize(); i++) {
-                            nonNullList.set(i, abstractMinecartContainerAccessor.driller$getItemStacks().get(i));
-                        }
-                        abstractMinecartContainerAccessor.driller$setItemStacks(nonNullList);
-                    }
-
                     if (!player.isCreative()) {
                         player.getItemInHand(interactionHand).shrink(1);
                     }
@@ -213,14 +168,6 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
                 } else if (this.hoppersInventory.getItem(0).isEmpty()) {
                     this.hoppersInventory.setItem(0, player.getItemInHand(interactionHand).copy());
                     this.entityData.set(LEFT_HOPPER, true);
-
-                    if (this instanceof AbstractMinecartContainerAccessor abstractMinecartContainerAccessor) {
-                        NonNullList<ItemStack> nonNullList = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-                        for (int i = 0; i < this.getContainerSize(); i++) {
-                            nonNullList.set(i, abstractMinecartContainerAccessor.driller$getItemStacks().get(i));
-                        }
-                        abstractMinecartContainerAccessor.driller$setItemStacks(nonNullList);
-                    }
 
                     if (!player.isCreative()) {
                         player.getItemInHand(interactionHand).shrink(1);
@@ -231,14 +178,6 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
                 this.hoppersInventory.setItem(0, player.getItemInHand(interactionHand).copy());
                 this.entityData.set(LEFT_HOPPER, true);
 
-                if (this instanceof AbstractMinecartContainerAccessor abstractMinecartContainerAccessor) {
-                    NonNullList<ItemStack> nonNullList = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-                    for (int i = 0; i < this.getContainerSize(); i++) {
-                        nonNullList.set(i, abstractMinecartContainerAccessor.driller$getItemStacks().get(i));
-                    }
-                    abstractMinecartContainerAccessor.driller$setItemStacks(nonNullList);
-                }
-
                 if (!player.isCreative()) {
                     player.getItemInHand(interactionHand).shrink(1);
                 }
@@ -246,14 +185,6 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
             } else if (this.hoppersInventory.getItem(1).isEmpty()) {
                 this.hoppersInventory.setItem(1, player.getItemInHand(interactionHand).copy());
                 this.entityData.set(RIGHT_HOPPER, true);
-
-                if (this instanceof AbstractMinecartContainerAccessor abstractMinecartContainerAccessor) {
-                    NonNullList<ItemStack> nonNullList = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-                    for (int i = 0; i < this.getContainerSize(); i++) {
-                        nonNullList.set(i, abstractMinecartContainerAccessor.driller$getItemStacks().get(i));
-                    }
-                    abstractMinecartContainerAccessor.driller$setItemStacks(nonNullList);
-                }
 
                 if (!player.isCreative()) {
                     player.getItemInHand(interactionHand).shrink(1);
@@ -272,8 +203,8 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
 
         Direction.Axis railAxis = getRailAxis(railState);
 
-        double forwardX = 0;
-        double forwardZ = 0;
+        double forwardX;
+        double forwardZ;
 
         if (railAxis == Direction.Axis.X) {
             forwardX = 1;
@@ -305,9 +236,7 @@ public abstract class MinecartHopperMixin extends AbstractMinecartContainer impl
 
     @Unique
     private boolean isNorthSouth(RailShape shape) {
-        return shape == RailShape.NORTH_SOUTH
-                || shape == RailShape.ASCENDING_NORTH
-                || shape == RailShape.ASCENDING_SOUTH;
+        return shape == RailShape.NORTH_SOUTH || shape == RailShape.ASCENDING_NORTH || shape == RailShape.ASCENDING_SOUTH;
     }
 
     @Override
